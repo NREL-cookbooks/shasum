b0fe98341f7d2f657fe4ed37ba2c3f9161ee0369
Nick Muerdter <nick.muerdter@nrel.gov>
2012-09-06 22:56:09 -0600

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Nick Muerdter"
+maintainer_email "nick.muerdter@nrel.gov"
+license          "MIT"
+description      "Installs/Configures shasum"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: shasum
+# Recipe:: default
+#
+# Copyright 2012, NREL
+#
+
+package "perl-Digest-SHA"
