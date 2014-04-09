5b31bcbead685931fe6557d2be9d80bdbeda977f
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-09 16:31:44 -0600

    Adding name to metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "shasum"
 maintainer       "Nick Muerdter"
 maintainer_email "nick.muerdter@nrel.gov"
 license          "MIT"
 description      "Installs/Configures shasum"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
